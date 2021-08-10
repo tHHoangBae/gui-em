9dbf46e825d8554c76e0cbd53f2a63d71432ae3f
buivinhk1 <65385174+buivinhk1@users.noreply.github.com>
2021-08-10 18:56:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Quynh à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introDesc: `Anh có chuyện này muốn hỏi em`,
+    btnIntro: 'Em đây',
+    title: 'Có phải em đã yêu anh từ cái nhìn đầu không 😙',
+    desc: 'Em có say anh không ',
+    btnYes: 'Em rất thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh đẹp trai em rất yêu anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh tới đón em đi ăn kem.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/buivinhk1' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
