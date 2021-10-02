86958e89b5a1a694d91a423eb859d0facf3799d2
tHHoangBae <91772630+tHHoangBae@users.noreply.github.com>
2021-10-02 13:49:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Quỳnh Mai bae ơi o((>ω< ))o !',
+    introTitle: 'Quỳnh Mai bae ơi o((>ω< ))o 💌!',
     introDesc: `ψ(｀∇´)ψ tui có chuyện này muốn hỏi bae nè `,
     btnIntro: 'Em đây',
     title: '(p≧w≦q) Có phải em rất yêu anh phải không bấybi 💘',
-    desc: 'Em có say anh không ',
-    btnYes: 'Em rất thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    desc: ' iuuu thì không được bấm không nghe chưa 💞 ',
+    btnYes: 'Tôi Rất Là Iu bạn Hoàng 💓 ',
+    btnNo: 'Không, Anh trai à💦',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Vì anh mập mạp dễ thương đó nòa  <33333333',
     mess: 'tui biết mà 🥰. iuuu bae của Hoàng nhiều nhiều nòa  😘😘',
-    messDesc: 'ヾ(•ω•`)o Thế Tối nay 7h mình cùng call ôn lý nhá .',
-    btnAccept: 'Okie lun <3',
+    messDesc: 'ヾ(•ω•`)o Thế Tối nay 7h mình cùng call ôn lý nhá 💌 .',
+    btnAccept: 'Okie lun 💝✔<3',
     messLink: 'https://www.facebook.com/profile.php?id=100026239419316' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
